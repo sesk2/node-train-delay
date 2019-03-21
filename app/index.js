4076ab38e41ce548ac510eb5f5ea6a22a56eacae
Sera <48615739+sesk2@users.noreply.github.com>
2019-03-21 20:05:42 +0900

    Update index.js

--- app/index.js
+++ app/index.js
@@ -4,17 +4,44 @@ const Delay = require('./train-delay');
 const myLine = new Line();
 const myDelay = new Delay();
 
+var CHECK_LIST = [
+  {
+    'name': '山手線',
+    'company': 'JR東日本',
+    'website': 'https://traininfo.jreast.co.jp/train_info/kanto.aspx#direction_yamate'
+  },
+  {
+    'name': '田園都市線',
+    'company': '東急電鉄',
+    'website': 'https://www.tokyu.co.jp/unten2/unten.html'
+  },
+]
+
+var check = 0;
+var names;
+
 // get LINE token from a environment[$LINE_TOKEN].
 myLine.setToken(process.env.LINE_TOKEN);
 
 // get information for train delaying
 myDelay.getDelayData().then(function(res){
     res.forEach(function(data){
-       // Send messages via LINE Notify
-       if(data.name == "東海道線" || data.name == "上野東京ライン"){
-           myLine.notify("現在、" + data.name + "が遅延しています。");
-       } else {
-           myLine.notify("現在、東海道線と上野東京ラインで遅延情報はありません。");
-       }
+      // Send messages via LINE Notify
+      CHECK_LIST.forEach(function(check_item){
+        if(data.name == check_item.name && data.company == check_item.company){
+          myLine.notify("現在、" + check_item.name + "が遅延しています。\n" + check_item.website);
+          check++;
+        }
+      });
     });
+    if(check == 0){
+      CHECK_LIST.forEach(function(check_item){
+        if(names){
+          names = names + " " + check_item.name;
+        } else {
+          names = check_item.name;
+        }
+      });
+      myLine.notify("現在、[" + names + "]で遅延情報はありません。");
+    }
 });
